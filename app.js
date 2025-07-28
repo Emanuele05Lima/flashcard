6e317e57bc7e67ee330b478f1a44c5c25c98ebbc
Emanuele05Lima <krassowski.emanuele@escola.pr.gov.br>
2025-07-28 11:59:58 -0300

    index

--- app.js
+++ app.js
@@ -14,3 +14,10 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
